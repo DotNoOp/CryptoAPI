74fd222a456382f013d6978188632907e8716be8
kolya5544 <05-kolya@bk.ru>
2021-10-01 12:00:40 +0300

    Optimization and improvement of code structure and algorithms

---
+++ Crypto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CryptoAPI
+{
+    class Crypto
+    {
+        public static string sha256(string v)
+        {
+            using (SHA256 m = SHA256.Create())
+            {
+                return BitConverter.ToString(m.ComputeHash(Encoding.UTF8.GetBytes(v))).ToLower().Replace("-", "");
+            }
+        }
+    }
+}
