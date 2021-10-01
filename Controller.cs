3de8ffc62b8c06e540cba8126336ef2952b6f6af
DotNoOp <a@nk.ax>
2021-10-01 11:39:06 +0300

    My first program

---
+++ Controller.cs
@@ -0,0 +1,43 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoAPI
+{
+    class Controller : WebApiController
+    {
+        public static async Task AsText(IHttpContext context, object? data)
+        {
+            if (data is null)
+            {
+                // Send an empty response
+                return;
+            }
+
+            context.Response.ContentType = MimeType.PlainText;
+            using var text = context.OpenResponseText(Encoding.UTF8);
+            // string.ToString returns the string itself
+            await text.WriteAsync((string)data).ConfigureAwait(false);
+        }
+
+        [Route(EmbedIO.HttpVerbs.Get, "/{val?}")]
+        public async Task<string> Hash(string val)
+        {
+            return md5(val);
+        }
+
+        public static string md5(string v)
+        {
+            using (MD5 m = MD5.Create())
+            {
+                return BitConverter.ToString(m.ComputeHash(Encoding.UTF8.GetBytes(v))).ToLower().Replace("-", "");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using EmbedIO;
+using EmbedIO.WebApi;
+using System;
+using System.Threading;
+
+namespace CryptoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateWebServer("http://127.0.0.1:80/").RunAsync();
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        private static WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                .WithCors()
+                .WithWebApi("/", Controller.AsText, m => m
+                    .WithController<Controller>());
+
+            return server;
+        }
+    }
+}
