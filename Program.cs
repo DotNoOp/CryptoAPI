14dec7553f39875e949c18ef6cffe128115a923d
GotToTop <zerozeronine@nk.ax>
2021-10-07 18:59:42 +0300

    CryptoAPI now works over TCP!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using EmbedIO;
 using EmbedIO.WebApi;
+using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Net;
@@ -29,8 +30,54 @@ namespace CryptoAPI
                 var sw = new StreamWriter(ns); sw.AutoFlush = true;
                 var sr = new StreamReader(ns);
 
-                string str = sr.ReadLine();
-                sw.WriteLine(Crypto.sha256(str));
+                string json = sr.ReadLine();
+
+                var p = JsonConvert.DeserializeObject<JSON.HashData>(json);
+
+                MemoryStream dataStream = new MemoryStream();
+                dataStream.Write(Convert.FromBase64String(p.contents));
+                if (!string.IsNullOrEmpty(p.salt)) dataStream.Write(Convert.FromBase64String(p.salt));
+                var data = dataStream.ToArray();
+
+                var response = new JSON.HashData();
+                response.method = p.method;
+                response.salt = p.salt;
+                response.key = p.key;
+                response.operation = p.operation;
+                response.dataLen = data.Length;
+
+                switch (p.method.ToLower())
+                {
+                    //hashing
+                    case "md5":
+                        response.contents = Crypto.md5(data);
+                        break;
+                    case "sha1":
+                    case "sha128":
+                        response.contents = Crypto.sha1(data);
+                        break;
+                    case "sha2":
+                    case "sha256":
+                    default:
+                        response.contents = Crypto.sha256(data);
+                        break;
+                    case "sha384":
+                        response.contents = Crypto.sha384(data);
+                        break;
+                    case "sha512":
+                        response.contents = Crypto.sha512(data);
+                        break;
+                    //encryption
+                    case "aes256":
+                        response.contents = Crypto.aes256(data, Convert.FromBase64String(p.key), p.operation);
+                        break;
+                    case "tripledes":
+                    case "tdes":
+                        response.contents = Crypto.TDES(data, Convert.FromBase64String(p.key), p.operation);
+                        break;
+                }
+
+                sw.WriteLine(JSON.Build(response));
             }
         }
 
